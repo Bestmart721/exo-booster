49866e05a73a735756ebbd521bab8648f8da9e1f
DESKTOP-NROUSJV\Winner <dynamomagnet@outlook.com>
2024-07-19 16:14:27 -0400

    chore: Update npm dependencies and add legacy plugin for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), legacy({
+    targets: ['chrome >= 49', 'defaults', 'not IE 11'],
+  }),],
 })
